42f982814b6c4ac83ab9274906898cef701d77b0
Felix Simkovic <felixsimkovic@me.com>
2017-10-04 08:58:31 +0100

    document structure slightly altered to allow for new or existing pyrvapi documents

--- __init__.py
+++ __init__.py
@@ -1,17 +1,17 @@
 # MIT License
-# 
+#
 # Copyright (c) 2017 Felix Simkovic
-# 
+#
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
 # in the Software without restriction, including without limitation the rights
 # to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 # copies of the Software, and to permit persons to whom the Software is
 # furnished to do so, subject to the following conditions:
-# 
+#
 # The above copyright notice and this permission notice shall be included in all
 # copies or substantial portions of the Software.
-# 
+#
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 # IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 # FITNESS FOR A :obj:PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -26,8 +26,8 @@ Examples
 
 1. Creating a document
 
-   >>> from rvapi import GuiDocument
-   >>> main = GuiDocument("GUI", "jsrview", "Main")
+   >>> from rvapi import NewDocument
+   >>> main = NewDocument("GUI", "jsrview", "Main")
 
 2. Create a tab
 
@@ -130,10 +130,16 @@ def Data(*args, **kwargs):
     return Data(*args, **kwargs)
 
 
-def GuiDocument(*args, **kwargs):
-    """A :obj:`GuiDocument <.document.GuiDocument>` class"""
-    from .document import GuiDocument
-    return GuiDocument(*args, **kwargs)
+def NewDocument(*args, **kwargs):
+    """A :obj:`NewDocument <.document.NewDocument>` class"""
+    from .document import NewDocument
+    return NewDocument(*args, **kwargs)
+
+
+def ExistingDocument(*args, **kwargs):
+    """A :obj:`ExistingDocument <.document.ExistingDocument>` class"""
+    from .document import ExistingDocument
+    return ExistingDocument(*args, **kwargs)
 
 
 def MockDocument(*args, **kwargs):
@@ -194,4 +200,3 @@ def Tree(*args, **kwargs):
     """A :obj:`Tree <.tree.Tree>` class"""
     from .tree import Tree
     return Tree(*args, **kwargs)
-
--- document.py
+++ document.py
@@ -1,17 +1,17 @@
 # MIT License
-# 
+#
 # Copyright (c) 2017 Felix Simkovic
-# 
+#
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
 # in the Software without restriction, including without limitation the rights
 # to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 # copies of the Software, and to permit persons to whom the Software is
 # furnished to do so, subject to the following conditions:
-# 
+#
 # The above copyright notice and this permission notice shall be included in all
 # copies or substantial portions of the Software.
-# 
+#
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 # IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 # FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -27,16 +27,38 @@ import os
 import pyrvapi
 
 from .decorator import rvapi_flush
-from .entity import Entity 
+from .entity import Entity
 
 
-class GuiDocument(Entity):
-
+class Document(Entity):
     CCP4_ENV = os.environ["CCP4"]
     SHARE_JSRVIEW = os.path.join(CCP4_ENV, "share", "jsrview")
 
+    def __init__(self):
+        super(Document, self).__init__(None)
+        self._identifier = None
+        self._title = None
+        self._mode = None
+        self._layout = None
+        self._jsrview_dir = None
+        self._help_fname = None
+        self._html_fname = None
+        self._task_fname = None
+        self._webserver_uri = None
+        self._meta = {}
+
+    @rvapi_flush
+    def refresh(self):
+        pass
+
+    def store(self):
+        pass
+
+
+class NewDocument(Document):
+
     def __init__(self, identifier, jsrview_dir, title, mode=1, layout=7, help_fname=None, html_fname=None, task_fname=None, webserver_uri=None):
-        super(GuiDocument, self).__init__(None)
+        super(NewDocument, self).__init__(None)
         self._identifier = identifier
         self._title = title
         self._mode = mode
@@ -46,39 +68,43 @@ class GuiDocument(Entity):
         self._html_fname = self.absolute_path(html_fname)
         self._task_fname = self.absolute_path(task_fname)
         self._webserver_uri = None
-        
+
         if not os.path.isdir(self._jsrview_dir):
             os.mkdir(self._jsrview_dir)
-        
+
         pyrvapi.rvapi_init_document(
-            self._identifier, self._jsrview_dir, self._title, self._mode, 
-            self._layout, self.SHARE_JSRVIEW, self._help_fname, self._html_fname, 
+            self._identifier, self._jsrview_dir, self._title, self._mode,
+            self._layout, self.SHARE_JSRVIEW, self._help_fname, self._html_fname,
             self._task_fname, None
-        ) 
-    
+        )
+
         if webserver_uri:
             self._webserver_start = len(jsrview_dir) + 1
         else:
             jsrview = os.path.join(self.CCP4_ENV, "libexec", "jsrview")
             if self._html_fname is None:
-                self._html_fname = os.path.join(self._jsrview_dir, "index.html")
+                self._html_fname = os.path.join(
+                    self._jsrview_dir, "index.html")
             import subprocess
             subprocess.Popen([jsrview, self._html_fname])
-        
+
         pyrvapi.rvapi_add_header(title)
         self.refresh()
-    
-    @rvapi_flush
-    def refresh(self):
-        pass
 
 
-class MockDocument(Entity):
-    
+class ExistingDocument(Document):
+
+    def __init__(self, identifier, html_fname=None, *args, **kwargs):
+        self._identifier = identifier
+        self._html_fname = html_fname
+        pyrvapi.rvapi_restore_document2(self._html_fname)
+
+    def store(self):
+        pyrvapi.rvapi_put_meta(self.meta)
+        pyrvapi.rvapi_store_document2(self._html_fname)
+
+
+class MockDocument(Document):
+
     def __init__(self, *args, **kwargs):
-        super(MockDocument, self).__init__(None)
-    
-    @rvapi_flush
-    def refresh(self):
-        pass
-
+        super(MockDocument, self).__init__()
